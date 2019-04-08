79be45a3104210dba6bfa62f3a6a610ec6186561
Notsunek <ber.ger@seznam.cz>
2019-04-08 12:50:41 +0200

    first exercise

---
+++ week-02/day-1/doubling.java
@@ -0,0 +1,20 @@
+public class Doubling {
+
+        // - Create an integer variable named `baseNum` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+        // - Print the result of `doubling(baseNum)`
+
+
+
+        public static int doubling(int x) {
+            return 2 * x;
+        }
+
+        public static void main(String[] args) {
+
+            int baseNum = 123;
+
+            System.out.println(doubling(baseNum));
+
+    }
+}

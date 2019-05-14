2e9e399d4db358279e971464e6b1564126ff5221
Notsunek <ber.ger@seznam.cz>
2019-05-14 16:47:11 +0200

    concat a character list to a string, but i am not sure, that it is right

---
+++ week-06/day-2/src/Exercise8.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+
+        //Write a Stream Expression to concatenate a Character list to a string!
+
+        String dog = "dog";
+        List<Character> list = Arrays.asList('h', 'o', 't');
+
+        String concatToString = list
+                .stream()
+                .map(c -> Character.toString((char)c))
+                .collect(Collectors.joining());
+
+        System.out.println(dog.concat(concatToString));
+    }
+}

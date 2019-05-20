e160b0448e09f861a17205f34658ad4a44c8c8b6
Notsunek <ber.ger@seznam.cz>
2019-05-20 14:38:18 +0200

    how to use utext in html.

--- week-07/day-1/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/Controller.java
+++ week-07/day-1/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/Controller.java
@@ -1,6 +1,25 @@
 package com.greenfoxacademy.bankofsimba.controller;
 
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
 public class Controller {
 
-    
+    BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+
+    @RequestMapping("/show")
+    public String show(Model model) {
+        model.addAttribute(bankAccount);
+        return "bankOfSimba";
+    }
+
+    @RequestMapping("/string")
+    public String string(Model model) {
+        model.addAttribute(bankAccount);
+        return "bankOfSimba";
+    }
 }
--- week-07/day-1/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
+++ week-07/day-1/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
@@ -3,7 +3,36 @@ package com.greenfoxacademy.bankofsimba.model;
 public class BankAccount {
 
     private String name;
-    private int balance;
+    private double balance;
     private String animalType;
 
+    public BankAccount(String name, int balance, String animalType) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
 }

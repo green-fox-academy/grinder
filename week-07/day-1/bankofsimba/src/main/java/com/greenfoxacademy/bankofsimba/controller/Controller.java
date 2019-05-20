2f6e8c78e5884ad129df7b8dee1a77a1bfc28cb4
Notsunek <ber.ger@seznam.cz>
2019-05-20 13:22:14 +0200

    new project bank of simba added

---
+++ week-07/day-1/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/Controller.java
@@ -0,0 +1,6 @@
+package com.greenfoxacademy.bankofsimba.controller;
+
+public class Controller {
+
+    
+}
---
+++ week-07/day-1/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.bankofsimba.model;
+
+public class BankAccount {
+
+    private String name;
+    private int balance;
+    private String animalType;
+
+}

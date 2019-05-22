43b65c47f0052e3630f26394b714b317da75ea9f
Notsunek <ber.ger@seznam.cz>
2019-05-22 17:16:25 +0200

    encode and decode the caesar

--- week-07/day-3/usefulUtilities/src/main/java/com/usefulutilities/controller/Controller.java
+++ week-07/day-3/usefulUtilities/src/main/java/com/usefulutilities/controller/Controller.java
@@ -4,6 +4,7 @@ import com.usefulutilities.model.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -19,6 +20,32 @@ public class Controller {
     public String emptyColorPage(Model model) {
         model.addAttribute("randomColor", utilityService.randomColor());
         return "emptyColorPage";
-        }
     }
+
+    @RequestMapping("/useful/email")
+    public String validator(Model model, @RequestParam String email) {
+
+        if (utilityService.validateEmail(email)) {
+            model.addAttribute("email", "<font color=\"green\">" + email + " is valid email address</font>");
+        } else {
+            model.addAttribute("email", "<font color=\"red\">" + email + " is not valid email address</font>");
+        }
+        return "emptyColorPage";
+    }
+
+    @RequestMapping("useful/encode")
+    public String encode(Model model, @RequestParam String text, @RequestParam int number) {
+        model.addAttribute("caesar", utilityService.caesar(text, number));
+        return "emptyColorPage";
+    }
+
+    @RequestMapping("useful/decode")
+    public String decode(Model model, @RequestParam String text, @RequestParam int number) {
+        model.addAttribute("caesar", utilityService.caesar(text, (-number)));
+        return "emptyColorPage";
+    }
+}
+
+
+
 
